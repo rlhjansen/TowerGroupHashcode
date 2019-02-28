981edbf8245c6e983be251b3b9a700670daa2119
Angelo Groot <angelogroot@hotmail.com>
2019-02-28 21:01:22 +0100

    random now works

---
+++ random_walk.py
@@ -0,0 +1,13 @@
+import classes
+import numpy.random as rand
+import random
+import read_object
+
+
+def random_walk(images):
+    slideshow = []
+    while images:
+        i = random.randint(0, len(images))
+        img = images.pop(i)
+        succes = False
+        while not success or
--- read_object.py
+++ read_object.py
@@ -1,4 +1,5 @@
 import classes
+import random
 
 def create_Image(file_name):
     """Create list of object Image from text file"""
@@ -46,6 +47,38 @@ def create_slides(imgs):
 
     return slides
 
+def create_random_slides(imgs):
+    """Create list of slides from a list of images"""
+    # Create slides
+    slides = [classes.slide() for i in range(len(imgs))]
+
+    order = [i for i in range(len(slides))]
+    random.shuffle(order)
+
+    count = 0
+    lonely = False
+    # Iterate over images to put them in the slides list
+    for i in order:
+        # Insert horizontal image
+        if imgs[i].orientation == "H":
+            slides[count].insert(imgs[i])
+            count += 1
+        # Insert two verticals if a lonely index is present
+        elif lonely is not False:
+            slides[count].insert(imgs[lonely])
+            slides[count].insert(imgs[i])
+            count += 1
+            lonely = False
+        # Store this lonely vertical image
+        else:
+            lonely = i
+
+    # Delete empty slides
+    del slides[count:]
+
+    return slides
+
+
 
 if __name__ == '__main__':
 
@@ -53,10 +86,7 @@ if __name__ == '__main__':
     # Create list of images
     imgs = create_Image(file_name)
     # Create list of slides in order of the file
-    slides = create_slides(imgs)
+    slides = create_random_slides(imgs)
     print(len(imgs), len(slides))
     print(slides[-2].tags())
     print(slides[-1].tags())
-
-
-
