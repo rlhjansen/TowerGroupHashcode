b3fb255311ad423d2869b1454a84cefff15b951d
rlhjansen <rlh.jansen@outlook.com>
2019-02-28 19:42:39 +0100

    visualize distributions

--- investigate_distr.py
+++ investigate_distr.py
@@ -1,22 +1,37 @@
 from collections import Counter
+import matplotlib.pyplot as plt
 
-f = open("data/a_example.txt", "r")
+fa = "data/a_example.txt"
+fb = "data/b_lovely_landscapes.txt"
+fc = "data/c_memorable_moments.txt"
+fd = "data/d_pet_pictures.txt"
+fe = "data/e_shiny_selfies.txt"
 
-f2 = open("data/b_lovely_landscapes.txt")
+files = [["a", fa], ["b", fb], ["c", fc], ["d", fd], ["e", fe]]
 
+def distr(file):
+    c = Counter()
+    f = open(file)
+    for line in f.readlines():
+        c.update(read_tags(line))
+    return c
 
-c = Counter()
 
 def read_tags(line):
     return line.split(" ")[2:]
-
-for line in f2.readlines():
-    c.update(read_tags(line))
 
 
 def lprint(iterable):
     for elem in iterable:
         print(elem)
 
-
-lprint(c.most_common()[:20])
+for f in files:
+    values = Counter([v for v in distr(f[1]).values()]).most_common()
+    xs = [v[1] for v in values]
+    ys = [v[0] for v in values]
+    plt.plot(xs, ys)
+    plt.yscale("log")
+    plt.title(f[0])
+    plt.ylabel("occurance of tag")
+    plt.xlabel("occurance of occurance")
+    plt.show()
